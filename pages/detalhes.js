c11600741c740504d24aace7688fd61bd02de0da
amyyblack <ars34@aluno.ifnmg.edu.br>
2025-11-08 17:51:27 +0000

    delete config

--- pages/detalhes.js
+++ pages/detalhes.js
@@ -1,4 +1,4 @@
-import { API_URL } from '../config.js';
+import { API_URL } from '../main.js';
 
 export async function Detalhes(container, id) {
   container.innerHTML = '<p>Carregando detalhes...</p>';
